0872f1627561fff972929d4deda4e6282c59efd5
Jorge Guzman <jorguzman100@gmail.com>
2020-04-07 19:35:52 -0500

    Portfolio and Contact completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,30 +2,33 @@
                 Home
 ==================================== */
 
-
-
-
 /* ================================= 
                 Portfolio
 ==================================== */
 
-const dropdownElement = document.getElementsByClassName('dropdown-item');
-const typeOfProjectSection = document.getElementsByClassName('projectTypes');
+const dropdownElement = document.getElementsByClassName("dropdown-item");
+const typeOfProjectSection = document.getElementsByClassName("projectTypes");
 
-
-for(let i = 0; i < dropdownElement.length; i++) {
-    dropdownElement[i].addEventListener('mouseover', () => {
-        for(h = 0; h < typeOfProjectSection.length; h++) {
-            typeOfProjectSection[h].style.display = 'block';
-        }
-        for(let j = 0; j < typeOfProjectSection.length; j++) {
-            if(typeOfProjectSection[j] !== typeOfProjectSection[i]) {
-                typeOfProjectSection[j].style.display = 'none';
-            }
-        }
-    });
+for (let i = 0; i < dropdownElement.length; i++) {
+  dropdownElement[i].addEventListener("mouseover", () => {
+    for (h = 0; h < typeOfProjectSection.length; h++) {
+      typeOfProjectSection[h].style.display = "block";
+      window.scrollTo(0, 0);
+    }
+    for (let j = 0; j < typeOfProjectSection.length; j++) {
+      if (typeOfProjectSection[j] !== typeOfProjectSection[i]) {
+        typeOfProjectSection[j].style.display = "none";
+      }
+    }
+  });
 }
 
+/* for (let a = 0; a < dropdownElement.length; a++) {
+  dropdownElement[a].addEventListener("click", () => {
+    alert("a-clicked");
+    window.scrollTo(0, 0);
+  });
+} */
 
 /* ================================= 
                 Contact
